1ff2713a97c312878c22588e3e893bed73f7bc03
Priyanthan Mahindaraj <priyanthan.mahindaraj@kochasoft.com>
2025-01-07 02:42:52 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ resource "aws_ecs_task_definition" "app_task" {
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = <<DEFINITION
 [
