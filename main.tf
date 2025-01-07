682aeab742a662019fc8fffd9b3192e34ca2500f
Priyanthan Mahindaraj <priyanthan.mahindaraj@kochasoft.com>
2025-01-07 11:50:22 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -3,29 +3,30 @@ provider "aws" {
 }
 
 resource "aws_ecr_repository" "app_repo" {
-  name = "my-app-2"
+  name = "apache-web-repo"
 }
 
 resource "aws_ecs_cluster" "app_cluster" {
-  name = "my-app-cluster"
+  name = "apache-web-cluster"
 }
 
 resource "aws_ecs_task_definition" "app_task" {
-  family                   = "my-app-task"
+  family                   = "apache-web-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = <<DEFINITION
 [
   {
-    "name": "my-app",
+    "name": "apache-container",
     "image": "${aws_ecr_repository.app_repo.repository_url}:latest",
     "portMappings": [
       {
         "containerPort": 80,
-        "hostPort": 80
+        "protocol": "tcp"
       }
     ]
   }
@@ -40,7 +41,7 @@ DEFINITION
 
 
 resource "aws_ecs_service" "app_service" {
-  name            = "my-app-service"
+  name            = "apache-web-service"
   cluster         = aws_ecs_cluster.app_cluster.id
   task_definition = aws_ecs_task_definition.app_task.arn
   desired_count   = 1
