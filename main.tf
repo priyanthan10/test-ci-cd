3007a4983e427275820be1e1c011662465b03221
Priyanthan Mahindaraj <priyanthan.mahindaraj@kochasoft.com>
2025-01-07 12:41:00 +0530

    updated terraform import aws_iam_role.ecs_task_execution_role ecsTaskExecutionRole

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+terraform import aws_iam_role.ecs_task_execution_role ecsTaskExecutionRole
+
 provider "aws" {
   region = var.region
 }
@@ -8,50 +10,6 @@ resource "aws_ecr_repository" "apache_web_repo" {
 
 resource "aws_ecs_cluster" "apache_web_cluster" {
   name = "apache-web-cluster"
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "ecs_task_execution_policy" {
-  name   = "ecsTaskExecutionPolicy"
-  role   = aws_iam_role.ecs_task_execution_role.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchGetImage",
-          "ecr:BatchCheckLayerAvailability"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
 }
 
 resource "aws_ecs_task_definition" "apache_web_task" {
